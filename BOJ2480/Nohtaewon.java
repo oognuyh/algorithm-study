756693ee413b40f5b305d9e426ebdaeed8e16cbb
Nohtaewon <gkgk713@naver.com>
2022-05-12 23:38:26 +0900

    fix BOJ2048 (#1)

--- BOJ2480/Nohtaewon.java
+++ BOJ2480/Nohtaewon.java
@@ -5,7 +5,7 @@ public class Main {
 	  Scanner sc= new Scanner(System.in);
 	  int num1, num2, num3;
 	  num1=sc.nextInt(); num2=sc.nextInt(); num3=sc.nextInt();
-	  if(num1==num2&&num2==num3&&num3==num1) {
+	  if(num1==num2&&num2==num3) {
 		  System.out.println(10000+num1*1000);
 	  }else if(num1==num2||num3==num1) {
 		  System.out.println(1000+num1*100);
