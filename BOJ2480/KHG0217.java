611532438bb1d1246f57504a99c585fc90925ed8
kim <hyukgyu3818@naver.com>
2022-05-13 12:51:26 +0900

    fix BOJ2480 (#1)

--- BOJ2480/KHG0217.java
+++ BOJ2480/KHG0217.java
@@ -1,29 +1,28 @@
-package test.main;
-
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-public class Main {
+public class KHG0217 {
 	 public static void main(String[] args) throws NumberFormatException, IOException {		   
 		 BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		 StringTokenizer st = new StringTokenizer(bf.readLine());
-		 	 
+ 
 		 int a =Integer.parseInt(st.nextToken());
 		 int b =Integer.parseInt(st.nextToken());
 		 int c =Integer.parseInt(st.nextToken());
-		 
-		
-			 if((a==b)&&(a==c)) {
-	 	         System.out.println(10000+a*1000);
-			 }else if((a==b)||(a==c)) {
-	 	         System.out.println(1000+a*100);
-	 	 	}else if((b==c)) {
-	 	         System.out.println(1000+b*100);
-	 	 	}else if((a!=b)&&(a!=c)) {
-	 	         int big = (a>b)&&(a>c)?a:(c>b?c:b);
-	 	         System.out.println(big*100);
-	 	 	}  		 	            		      
-	 }
-}
+		 	 
+	      if((a==b)&&(a==c)) {
+	         System.out.println(10000+a*1000);
+	      }else if((a==b)||(a==c)) {
+	         System.out.println(1000+a*100); 
+	      }else if((b==c)) {
+	         System.out.println(1000+b*100);
+	      }else if((a!=b)&&(a!=c)) {
+	    	  int big = Math.max(a,Math.max(b, c));
+		         System.out.println(big*100);
+	      }
+	   }
+	}
