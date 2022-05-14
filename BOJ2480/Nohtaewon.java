f5afd3f6437325ae61dde1b6f463780d4aee23c8
Hyungoo Kang <oognuyh@gmail.com>
2022-05-14 20:12:18 +0900

    change push to pull request

--- BOJ2480/Nohtaewon.java
+++ BOJ2480/Nohtaewon.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class Nohtaewon {
    public static void main(String[] args) {
 	  Scanner sc= new Scanner(System.in);
 	  int num1, num2, num3;
