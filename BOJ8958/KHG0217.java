ad735978d77113a75d077a011b13669b76c7c271
KHG <104048790+KHG0217@users.noreply.github.com>
2022-05-17 15:41:29 +0900

    solve BOJ8958 (#38) (#40)

---
+++ BOJ8958/KHG0217.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
+
+
+public class KHG0217 {
+	 public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));		
+		int count=Integer.parseInt(br.readLine());		
+
+		
+			for(int i=0; i<count; i++) {			
+				
+				int jumsu=0;
+				int sum=0;
+				String s=br.readLine();						
+				for(int j=0; j<s.length(); j++) {																								
+					char c =s.charAt(j);
+					if(c == 'O') {										
+						jumsu +=++sum;
+					}else {
+							sum=0;
+					 }						
+				}
+				System.out.println(jumsu);
+
+			}					
+
+	}
+		
+}
