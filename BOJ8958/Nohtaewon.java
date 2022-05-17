3f6f7909c0027965efc2a55f3b389ac710b10cdd
taewon <104048851+Nohtaewon@users.noreply.github.com>
2022-05-17 21:59:02 +0900

    solve BOJ8959 (#38) (#42)

---
+++ BOJ8958/Nohtaewon.java
@@ -0,0 +1,36 @@
+package BOJ8958;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Nohtaewon {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int tc=Integer.parseInt(br.readLine());
+		
+
+		for(int i=0;i<tc;i++) {
+			int jumsu=0;
+			int jumsu2=0;
+			String ox=br.readLine();
+			for(int j=0;j<ox.length(); j++) {
+				if(ox.charAt(j)=='O') {
+					jumsu2++;
+					jumsu+=jumsu2;
+				}else {
+					jumsu2=0;
+				}
+				
+			}
+			bw.write(jumsu+"\n");
+			
+		}
+		bw.close();
+		br.close();
+		
+	}
+}
