552f9569c124f95b01aabb3a5581aecc10b8ffad
oognuyh <oognuyh@users.noreply.github.com>
2022-05-17 13:47:42 +0000

    reformat java files automatically

--- BOJ8958/oognuyh.java
+++ BOJ8958/oognuyh.java
@@ -9,31 +9,27 @@ import java.util.stream.IntStream;
 
 class oognuyh {
 
-    public static void main(String[] args) throws IOException {
-        try (
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
-        ) {
-            int numOfCases = Integer.parseInt(bufferedReader.readLine().trim());
-            int[] acc = new int[] { 0 };
+  public static void main(String[] args) throws IOException {
+    try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out)); ) {
+      int numOfCases = Integer.parseInt(bufferedReader.readLine().trim());
+      int[] acc = new int[] {0};
 
-            for (int i = 0; i < numOfCases; i++) {
-                String ox = bufferedReader.readLine().trim();
+      for (int i = 0; i < numOfCases; i++) {
+        String ox = bufferedReader.readLine().trim();
 
-                bufferedWriter.write(
-                    IntStream
-                        .range(0, ox.length())
-                        .map((j) -> {
-                            acc[0] = ox.charAt(j) == 'O' ?
-                                acc[0] + 1 :
-                                0;
-                            return acc[0];
+        bufferedWriter.write(
+            IntStream.range(0, ox.length())
+                    .map(
+                        (j) -> {
+                          acc[0] = ox.charAt(j) == 'O' ? acc[0] + 1 : 0;
+                          return acc[0];
                         })
-                        .sum() + "\n"
-                );
+                    .sum()
+                + "\n");
 
-                acc[0] = 0;
-            }
-        }
+        acc[0] = 0;
+      }
     }
+  }
 }
