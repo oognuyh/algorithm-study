8bc43f94f328aaf84423477c7df51a4bb88176ea
oognuyh <oognuyh@users.noreply.github.com>
2022-05-21 06:57:12 +0000

    reformat java files automatically

--- P72410/Nohtaewon.java
+++ P72410/Nohtaewon.java
@@ -1,23 +1,22 @@
 package P72410;
 
 class Nohtaewon {
-	public String solution(String new_id){
-		String answer=new_id;
-		answer=answer.toLowerCase();
-		answer=answer.replaceAll("[^a-z0-9-_.]","");
-		answer=answer.replaceAll("\\.{2,}", ".");
-		answer=answer.replaceAll("^\\.|\\.$","");
-		if(answer.equals("")) {
-			answer="a";
-		}
-		if(answer.length()>=16) {
-			answer=answer.substring(0,15);
-			answer=answer.replaceAll("\\.$","");
-		}
-        while(answer.length()<3){
-            answer+=answer.substring(answer.length()-1);
-        }
-		return answer;
-		
-	}
+  public String solution(String new_id) {
+    String answer = new_id;
+    answer = answer.toLowerCase();
+    answer = answer.replaceAll("[^a-z0-9-_.]", "");
+    answer = answer.replaceAll("\\.{2,}", ".");
+    answer = answer.replaceAll("^\\.|\\.$", "");
+    if (answer.equals("")) {
+      answer = "a";
+    }
+    if (answer.length() >= 16) {
+      answer = answer.substring(0, 15);
+      answer = answer.replaceAll("\\.$", "");
+    }
+    while (answer.length() < 3) {
+      answer += answer.substring(answer.length() - 1);
+    }
+    return answer;
+  }
 }
