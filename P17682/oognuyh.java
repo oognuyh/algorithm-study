ab06dd61d595362578824a470e990489d09f3b60
Hyungoo Kang <oognuyh@gmail.com>
2022-05-25 23:31:28 +0900

    reformat java files automatically (#66)
    Co-authored-by: oognuyh <oognuyh@users.noreply.github.com>

--- P17682/oognuyh.java
+++ P17682/oognuyh.java
@@ -5,34 +5,33 @@ import java.util.Map;
 
 public class oognuyh {
 
-    public int solution(String dartResult) {
-        Map<Character, Integer> mapper = Map.of(
+  public int solution(String dartResult) {
+    Map<Character, Integer> mapper =
+        Map.of(
             'S', 1,
             'D', 2,
             'T', 3,
             '*', 2,
-            '#', -1
-        );
+            '#', -1);
 
-        int[] scores = new int[3];
-        StringBuilder sb = new StringBuilder(dartResult);
+    int[] scores = new int[3];
+    StringBuilder sb = new StringBuilder(dartResult);
 
-        for (int round = 0; round < 3; round++) {
-            int endIndex = sb.substring(0, 2).equals("10") ? 2 : 1;
-            scores[round] = Integer.parseInt(sb.substring(0, endIndex));
-            sb.delete(0, endIndex);
+    for (int round = 0; round < 3; round++) {
+      int endIndex = sb.substring(0, 2).equals("10") ? 2 : 1;
+      scores[round] = Integer.parseInt(sb.substring(0, endIndex));
+      sb.delete(0, endIndex);
 
-            scores[round] = (int) Math.pow(scores[round], mapper.get(sb.charAt(0)));
-            sb.delete(0, 1);
+      scores[round] = (int) Math.pow(scores[round], mapper.get(sb.charAt(0)));
+      sb.delete(0, 1);
 
-            if (sb.length() > 0 && mapper.containsKey(sb.charAt(0))) {
-                scores[round] *= mapper.get(sb.charAt(0));
-                if (sb.charAt(0) == '*' && round > 0) scores[round - 1] *= mapper.get(sb.charAt(0));
-                sb.delete(0, 1);
-            } 
-        }        
-
-        return Arrays.stream(scores)
-            .sum();
+      if (sb.length() > 0 && mapper.containsKey(sb.charAt(0))) {
+        scores[round] *= mapper.get(sb.charAt(0));
+        if (sb.charAt(0) == '*' && round > 0) scores[round - 1] *= mapper.get(sb.charAt(0));
+        sb.delete(0, 1);
+      }
     }
+
+    return Arrays.stream(scores).sum();
+  }
 }
