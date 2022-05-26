9fb6d578cc40c02485841d6aa725092d663ba6ba
Hyungoo Kang <oognuyh@gmail.com>
2022-05-26 23:14:33 +0900

    reformat java files automatically (#73)
    Co-authored-by: oognuyh <oognuyh@users.noreply.github.com>

--- P12982/oognuyh.java
+++ P12982/oognuyh.java
@@ -4,12 +4,10 @@ import java.util.Arrays;
 
 class oognuyh {
 
-    public int solution(int[] d, int budget) {
-        int[] total = { 0 };
+  public int solution(int[] d, int budget) {
+    int[] total = {0};
 
-        return (int) Arrays.stream(d)
-            .sorted()
-            .takeWhile((amount) -> (total[0] += amount) <= budget)
-            .count();
-    }
+    return (int)
+        Arrays.stream(d).sorted().takeWhile((amount) -> (total[0] += amount) <= budget).count();
+  }
 }
