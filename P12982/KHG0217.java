a6a7c16ba6062a167166e9220016b736016c8557
oognuyh <oognuyh@users.noreply.github.com>
2022-05-26 14:13:18 +0000

    reformat java files automatically

--- P12982/KHG0217.java
+++ P12982/KHG0217.java
@@ -3,25 +3,23 @@ package P12982;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Iterator;
 
 class KHG0217 {
-	public int solution(int[] d, int budget) {
-		int answer = 0;
-		List<Integer> set1 = new ArrayList<>();
-		for (int i = 0; i < d.length; i++) {
-			set1.add(d[i]);
-		}
+  public int solution(int[] d, int budget) {
+    int answer = 0;
+    List<Integer> set1 = new ArrayList<>();
+    for (int i = 0; i < d.length; i++) {
+      set1.add(d[i]);
+    }
 
-		Collections.sort(set1);
+    Collections.sort(set1);
 
-		for (int i = 0; i < set1.size(); i++) {
-			if (budget - set1.get(i) >= 0) {
-				budget = budget - set1.get(i);
-				answer++;
-			}
-		}
-		return answer;
-	}
-
+    for (int i = 0; i < set1.size(); i++) {
+      if (budget - set1.get(i) >= 0) {
+        budget = budget - set1.get(i);
+        answer++;
+      }
+    }
+    return answer;
+  }
 }
