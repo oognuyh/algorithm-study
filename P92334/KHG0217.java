db67d7e10071606b86067da937f3c01a1c60ca6a
Hyungoo Kang <oognuyh@gmail.com>
2022-05-27 22:48:09 +0900

    reformat java files automatically (#80)
    Co-authored-by: oognuyh <oognuyh@users.noreply.github.com>

--- P92334/KHG0217.java
+++ P92334/KHG0217.java
@@ -5,40 +5,38 @@ import java.util.HashSet;
 import java.util.Map;
 
 class KHG0217 {
-	public static int[] solution(String[] id_list, String[] report, int k) {
-		int[] answer = new int[id_list.length];
+  public static int[] solution(String[] id_list, String[] report, int k) {
+    int[] answer = new int[id_list.length];
 
+    Map<String, HashSet<String>> idMap = new HashMap<>();
+    Map<String, Integer> answerMap = new HashMap<>();
 
-		Map<String, HashSet<String>> idMap = new HashMap<>();
-		Map<String, Integer> answerMap = new HashMap<>();
+    for (int i = 0; i < id_list.length; i++) {
+      HashSet<String> reportingId = new HashSet<>();
+      idMap.put(id_list[i], reportingId);
+      answerMap.put(id_list[i], 0);
+    }
 
-		for (int i = 0; i < id_list.length; i++) {
-			HashSet<String> reportingId = new HashSet<>();
-			idMap.put(id_list[i], reportingId);
-			answerMap.put(id_list[i], 0);
-		}
+    for (String s : report) {
+      String[] strDivide = s.split(" ");
+      String reportingId = strDivide[0];
+      String reportedId = strDivide[1];
+      idMap.get(reportedId).add(reportingId);
+    }
 
-		for (String s : report) {
-			String[] strDivide = s.split(" ");
-			String reportingId = strDivide[0];
-			String reportedId = strDivide[1];
-			idMap.get(reportedId).add(reportingId);
-		}
+    for (String reportedId : idMap.keySet()) {
+      HashSet<String> reportingId = idMap.get(reportedId);
+      if (reportingId.size() >= k) {
+        for (String userId : reportingId) {
+          answerMap.put(userId, answerMap.get(userId) + 1);
+        }
+      }
+    }
 
-		for (String reportedId : idMap.keySet()) {
-			HashSet<String> reportingId = idMap.get(reportedId);
-			if (reportingId.size() >= k) {
-				for (String userId : reportingId) {
-					answerMap.put(userId, answerMap.get(userId) + 1);
-				}
-			}
-		}
+    for (int i = 0; i < id_list.length; i++) {
+      answer[i] = answerMap.get(id_list[i]);
+    }
 
-		for (int i = 0; i < id_list.length; i++) {
-			answer[i] = answerMap.get(id_list[i]);
-		}
-
-		return answer;
-	}
-
+    return answer;
+  }
 }
