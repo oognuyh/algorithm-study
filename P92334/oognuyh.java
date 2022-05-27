a48e5eb203b1b762851643ca785d7ade2315f1c5
oognuyh <oognuyh@users.noreply.github.com>
2022-05-27 03:57:49 +0000

    reformat java files automatically

--- P92334/oognuyh.java
+++ P92334/oognuyh.java
@@ -5,19 +5,23 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class oognuyh {
-    
-    public int[] solution(String[] id_list, String[] report, int k) {   
-        Map<String, Long> numOfEmails = Arrays.stream(report)
+
+  public int[] solution(String[] id_list, String[] report, int k) {
+    Map<String, Long> numOfEmails =
+        Arrays.stream(report)
             .distinct()
-            .collect(Collectors.groupingBy((history) -> history.split(" ")[1], 
-                Collectors.mapping((history) -> history.split(" ")[0], Collectors.toList())))
-            .entrySet().stream()
+            .collect(
+                Collectors.groupingBy(
+                    (history) -> history.split(" ")[1],
+                    Collectors.mapping((history) -> history.split(" ")[0], Collectors.toList())))
+            .entrySet()
+            .stream()
             .filter((entry) -> entry.getValue().size() >= k)
             .flatMap((entry) -> entry.getValue().stream())
             .collect(Collectors.groupingBy((id) -> id, Collectors.counting()));
-            
-        return Arrays.stream(id_list)
-            .mapToInt((id) -> numOfEmails.getOrDefault(id, 0L).intValue())
-            .toArray();
-    }
+
+    return Arrays.stream(id_list)
+        .mapToInt((id) -> numOfEmails.getOrDefault(id, 0L).intValue())
+        .toArray();
+  }
 }
