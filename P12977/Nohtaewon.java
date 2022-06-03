5752863b53093dd9c571f366911373f5aac93500
Nohtaewon <gkgk713@naver.com>
2022-06-03 21:52:23 +0900

    fix P12977 (#83)

--- P12977/Nohtaewon.java
+++ P12977/Nohtaewon.java
@@ -1,26 +1,21 @@
 package P12977;
 
 class Nohtaewon{
-	public int solution(int[] nums) {
+    public boolean isPrime(int n){
+        for(int i=2;i*i<=n;i++){
+            if(n%i==0) return false;
+        }
+        return true;
+    }
+    public int solution(int[] nums) {
 		int answer=0;
 		for(int i=0; i<nums.length;i++) {
 			for(int j=i+1;j<nums.length;j++) {
 				for(int k=j+1;k<nums.length;k++) {
-					int sum=nums[i]+nums[j]+nums[k];
-					int count=0;
-					for(int l=2;l<=sum;l++) {
-						if(sum%l==0) {
-							count++;
-						}else if(count>1) {
-							break;
-						}
-					}
-					if(count==1) {
-						answer++;
-					}
+					if(isPrime(nums[i]+nums[j]+nums[k])) answer++;
 				}
 			}
 		}
-		return answer;		
-	}
+        return answer;
+    }
 }
