7155dddd01dc1c93f46de80b996a7483d9f06af0
Hyungoo Kang <oognuyh@gmail.com>
2022-06-05 23:53:53 +0900

    reformat java files automatically (#86)
    Co-authored-by: oognuyh <oognuyh@users.noreply.github.com>

--- P12977/oognuyh.java
+++ P12977/oognuyh.java
@@ -3,24 +3,23 @@ package P12977;
 import java.util.Arrays;
 
 public class oognuyh {
-    
-    public int solution(int[] nums) {
-        boolean[] isPrime = new boolean[1000 + 999 + 998 + 1];
-        int numOfPrimes = 0;
 
-        Arrays.fill(isPrime, true);
+  public int solution(int[] nums) {
+    boolean[] isPrime = new boolean[1000 + 999 + 998 + 1];
+    int numOfPrimes = 0;
 
-        isPrime[0] = isPrime[1] = false;
+    Arrays.fill(isPrime, true);
 
-        for (int i = 2; i * i < isPrime.length; i++) 
-            if (isPrime[i]) 
-                for (int j = 2; i * j < isPrime.length; j++) isPrime[i * j] = false;
+    isPrime[0] = isPrime[1] = false;
 
-        for (int i = 0; i < nums.length; i++) 
-            for (int j = i + 1; j < nums.length; j++) 
-                for (int k = j + 1; k < nums.length; k++) 
-                    if (isPrime[nums[i] + nums[j] + nums[k]]) numOfPrimes++;
+    for (int i = 2; i * i < isPrime.length; i++)
+      if (isPrime[i]) for (int j = 2; i * j < isPrime.length; j++) isPrime[i * j] = false;
 
-        return numOfPrimes;
-    }
+    for (int i = 0; i < nums.length; i++)
+      for (int j = i + 1; j < nums.length; j++)
+        for (int k = j + 1; k < nums.length; k++)
+          if (isPrime[nums[i] + nums[j] + nums[k]]) numOfPrimes++;
+
+    return numOfPrimes;
+  }
 }
