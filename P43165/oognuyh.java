1e8d93a3f130526624c03336b0caf0b0926877f0
Hyungoo Kang <oognuyh@gmail.com>
2022-06-12 23:04:14 +0900

    solve P43165 (#88) (#90)

---
+++ P43165/oognuyh.java
@@ -0,0 +1,15 @@
+package P43165;
+
+public class oognuyh {
+
+    public int dfs(int[] numbers, int target, int index) {
+        return index < numbers.length
+                ? dfs(numbers, target - numbers[index], index + 1)
+                        + dfs(numbers, target + numbers[index], index + 1)
+                : target == 0 ? 1 : 0;
+    }
+
+    public int solution(int[] numbers, int target) {
+        return dfs(numbers, target, 0);
+    }
+}
