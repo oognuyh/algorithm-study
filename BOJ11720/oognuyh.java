4e9d1b7416d5221a9312d889e9380183c772c26d
Hyungoo Kang <oognuyh@gmail.com>
2022-07-16 20:11:22 +0900

    solve #93

---
+++ BOJ11720/oognuyh.java
@@ -0,0 +1,18 @@
+package BOJ11720;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+class oognuyh {
+
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
+            bufferedReader.readLine().trim();
+
+            System.out.println(Arrays.stream(bufferedReader.readLine().trim().split(""))
+                    .mapToInt(Integer::parseInt).sum());
+        }
+    }
+}
